fb4d7671dc1f23149daeee094b5a3cdd17ce8f9d
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2019-12-20 17:09:55 +0100

    debugging

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const { mongoURI, cookieKey } = require("./config/keys");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
 
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log(`Connected to remote MongoDB...`))
+  .catch(err => console.log("error is: ", err.message));
+
 require("./models/User");
-require("./services/passport")();
+require("./services/passport");
 const app = express();
 
 app.use(
@@ -20,13 +25,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
-mongoose
-  .connect(mongoURI)
-  .then(() => console.log(`Connected to remote MongoDB...`))
-  .catch(err => console.log("error is: ", err.message));
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening on PORT ${PORT}...`);
 });
-// connection string: mongodb://victor:12345@cluster0-shard-00-00-ooqtx.mongodb.net:27017,cluster0-shard-00-01-ooqtx.mongodb.net:27017,cluster0-shard-00-02-ooqtx.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,7 @@ const passport = require("passport");
 const keys = require("../config/keys");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
+
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
@@ -13,27 +14,25 @@ passport.deserializeUser(async (id, done) => {
   done(null, user);
 });
 
-module.exports = function() {
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleSecretKey,
-        callbackURL: "/auth/google/callback",
-        proxy: true
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id });
-        if (existingUser) {
-          // we already have the user with the given ID
-          done(null, existingUser);
-        } else {
-          // we dont have a record, make a new user
-          new User({ googleId: profile.id });
-          const user = await user.save();
-          done(null, user);
-        }
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleSecretKey,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // we already have the user with the given ID
+        done(null, existingUser);
+      } else {
+        // we dont have a record, make a new user
+
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
       }
-    )
-  );
-};
+    }
+  )
+);
