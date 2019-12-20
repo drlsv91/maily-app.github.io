65d528687f5804895b8ec1a42644ce6f234a2e21
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2019-12-20 15:59:23 +0100

    fixes error

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,9 @@
 // prod.js - production keys here
-odule.exports = {
+module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleSecretKey: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
+
+// mongodb://victor-prod:12345@cluster0-shard-00-00-g6yqf.mongodb.net:27017,cluster0-shard-00-01-g6yqf.mongodb.net:27017,cluster0-shard-00-02-g6yqf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
