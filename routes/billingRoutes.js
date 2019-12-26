9891d87c5fe9268dd3d40c1efe7c09630bba035d
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2019-12-26 16:55:19 +0100

    Added billing and client side app

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,1 +1,17 @@
-module.exports = app => {};
+const { stripeSecretKey } = require("../config/keys");
+const requireLogin = require("../middlewares/requireLogin");
+const stripe = require("stripe")(stripeSecretKey);
+module.exports = app => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+    await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 credits",
+      source: req.body.id
+    });
+    console.log(req);
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
+  });
+};
