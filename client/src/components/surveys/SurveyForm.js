786f04ec3adc932b6064d8295afa97892e6b89a1
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2019-12-29 17:15:11 +0100

    styling the form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,35 +1,30 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
+import { Link } from "react-router-dom";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" }
+];
 
 class SurveyForm extends Component {
   renderFields() {
     return (
       <div>
-        <Field
-          label="Survey Title"
-          type="text"
-          name="title"
-          component={SurveyField}
-        />
-        <Field
-          label="Subject Line"
-          type="text"
-          name="subject"
-          component={SurveyField}
-        />
-        <Field
-          label="Email Body"
-          type="text"
-          name="body"
-          component={SurveyField}
-        />
-        <Field
-          label="Recipient List"
-          type="text"
-          name="emails"
-          component={SurveyField}
-        />
+        {FIELDS.map(({ label, name }) => {
+          return (
+            <Field
+              key={name}
+              label={label}
+              type="text"
+              name={name}
+              component={SurveyField}
+            />
+          );
+        })}
       </div>
     );
   }
@@ -38,8 +33,12 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit" className="btn red">
-            Submit
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            <i className="material-icons right">done</i>
+            Next
           </button>
         </form>
       </div>
